261c84c5dedc8df0059b45b7b35f77e2fb33b2c3
nemanja.karaklajic <nemanjakaraklajic@teletrader.com>
2025-05-14 15:26:55 +0200

    Update tab width to 4 spaces in .prettierrc and index.js
    Adjusted the `.prettierrc` configuration to use a tab width of 4 spaces instead of 2. Reformatted `index.js` to align with the new tab width setting for consistent code style.

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 console.log('Happy developing ✨');
 
 const outer = function () {
-  let myAge = 35;
+    let myAge = 35;
 
-  function inner() {
-    const currentYear = 2025;
+    function inner() {
+        const currentYear = 2025;
 
-    // console.log(calculateBirthYear); // Error: if const: Cannot access 'calculateBirthYear' before initialization
-    // console.log(calculateBirthYear); // Error: if var: Undefined
+        // console.log(calculateBirthYear); // Error: if const: Cannot access 'calculateBirthYear' before initialization
+        // console.log(calculateBirthYear); // Error: if var: Undefined
 
-    const calculateBirthYear = currentYear - myAge;
-    // var calculateBirthYear = currentYear - myAge;
+        const calculateBirthYear = currentYear - myAge;
+        // var calculateBirthYear = currentYear - myAge;
 
-    console.log(`You are born in ${calculateBirthYear}`);
-  }
+        console.log(`You are born in ${calculateBirthYear}`);
+    }
 
-  inner();
+    inner();
 };
 
 outer();
